0ae565e28277aeea338af54786a1a0bbc94d85ab
Kevin Barroso <kobo.devops@gmail.com>
2022-08-07 19:15:18 -0300

    api get para documentos

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+Doc = Query()
 
 class documento(dict):
     def __init__(self, *args):
@@ -26,16 +27,65 @@ class MyTiny:
         self.db = TinyDB('db/db.json')
         self.docs = self.db.table('docs')
 
-    def _new_document(self,data) -> int :
+    def _new_document(self,data : dict) -> int :
         """
         Esta funcion instancia la clase documento, de la cual puede
         insertar documento en la database exportando en dict. \n 
         Retorna el id de dicho documento insertado.
         """
         doc = documento(data)
-        id = self.docs.insert(doc.__dict__)
-        return id
-
+        return self.docs.insert(doc.__dict__)
+    
+    def _get_document(self, NumDoc : str) -> dict:
+        """
+        Esta funcion comprueba si el documento existe, 
+        y lo retorna en una lista.
+        """
+        doc = self.docs.search(Doc.NumDoc == NumDoc)
+        if doc:
+            return doc
 
 db = MyTiny()
-print(db._new_document({'name':'kevin','lastname':'barroso'}))
+doc = {
+    'NumDoc':'EEPL-CAREM25C-220',
+    'Section':'Estructural',
+    'Title': 'Linea estructural H - +10',
+    'Revs':[
+        {
+            'Rev': 0,
+            'Date': '2022-02-01',
+            'OS':250,
+            'TotalSheets': 1,
+            'Status': 'APROBADO LIBERADO',
+            'Sheets':[
+                {
+                    'Sheet': 1,
+                    'Format': 'A1',
+                    'Ric':[
+                        {
+                            'Rev': 'A',
+                            'Date':'2022-02-14',
+                            'NumNota':[1,2]
+                        }
+                    ],
+                    'DQAs':[
+                        {
+                            'Nota':1,
+                            'NumDoc':'NCO-141',
+                            'Status':'PARA EJECUCION',
+                            'Verificacion':'SIN EJECUTAR'
+                        },
+                        {
+                            'Nota':2,
+                            'NumDoc':'AT-CAREM25OT-104',
+                            'Rev':0,
+                            'Status':'APROBADO LIBERADO',
+                            'Verificacion':'VERIFICADO'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+print(db._get_document('EEPL-CAREM25C-220'))
