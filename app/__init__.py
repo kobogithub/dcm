bbb27f2cb6f6bc3ca053b0e4bd40e0d9697fdd65
Kevin Barroso <kobo.devops@gmail.com>
2022-08-16 07:15:15 -0300

    requirements and init

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carem.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
@@ -10,4 +9,3 @@ db = SQLAlchemy(app)
 #login_manager = LoginManager(app)
 #login_manager.login_view = "login_page"
 #login_manager.login_message_category = "info"
-from app import routes
