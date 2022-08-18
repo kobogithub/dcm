0860f7710356cbecd3c1da4b374414a58c51bfbd
Kevin Barroso <kobo.devops@gmail.com>
2022-08-18 20:59:44 -0300

    requirimientos en forms

--- app/mod_cp/forms.py
+++ app/mod_cp/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
-from wtforms.validators import InputRequired, Length
+                     RadioField, SubmitField)
+from wtforms.validators import DataRequired, Length
 
 class DocumentForm(FlaskForm):
-    docnum = StringField('Codigo de Documento', validators=[InputRequired()])
+    docnum = StringField('Codigo de Documento', validators=[DataRequired(), Length(max=50)])
     doccnea = StringField('Codigo CNEA')
     docqbnet = StringField('Codigo QBNet')
-    title = StringField('Titulo', validators=[InputRequired()])
-    section = StringField('Seccion', validators=[InputRequired()])
+    title = TextAreaField('Titulo', validators=[DataRequired()])
+    section = StringField('Seccion', validators=[DataRequired()])
+
+    submit = SubmitField('Registrar')
